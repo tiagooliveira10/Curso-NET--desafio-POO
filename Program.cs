41681881d275aebfb54dd74034ab8cd9a14457c9
Tiago Oliveira <tiagosilva.oliveira@outlook.com.br>
2022-11-12 13:38:25 -0300

    Desafio POO concluido.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+//Testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "43988235491", modelo: "Nokia", imei:"11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "43912345678", modelo: "Iphone", imei: "2222222", memoria: 32);
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook");
